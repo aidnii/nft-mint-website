edadbb2e06a7c69f24c1da38ac6081bc614477d9
cryptichic <syberchief@gmail.com>
2022-08-03 20:04:09 -0500

    added setIsPublicMintEnabled, setBaseTokenURI, tokenURI, and withdraw functionalities

---
+++ nft-mint-website/contracts/RoboPunksNFT.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract RoboPunksNFT is ERC721, Ownable {
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenURI;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721('RoboPunks', 'RP') {
+        mintPrice = 0.02 ether;
+        totalSupply = 0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+        // set withdrawal wallet addresss
+    }
+
+    function  setIsPublicMintEnabled(bool _isPublicMintEnabled) external onlyOwner {
+        isPublicMintEnabled = _isPublicMintEnabled;
+    }
+
+    function setBaseTokenURI(string calldata _baseTokenURI) external onlyOwner {
+        baseTokenURI = _baseTokenURI;
+    }
+
+    function tokenURI(uint256 _tokenID) public view override returns (string memory) {
+        require(_exists(_tokenID), 'Token does not exist!');
+        return string(abi.encodePacked(baseTokenURI, Strings.toString(_tokenID), ".json"));
+    }
+
+    function withdraw() external onlyOwner {
+        (bool success, ) = withdrawWallet.call{ value: address(this).balance }('');
+        require(success, 'withdraw failed');
+    }
+}
+
